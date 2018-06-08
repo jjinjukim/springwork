b1cd1a3e746da29a36a51d0799d404e055466642
j <j@192.168.0.102>
2018-06-08 16:46:29 +0900

    hello수정

--- GitTestJava/src/Test.java
+++ GitTestJava/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello33");
 		System.out.println("hello2");
 		System.out.println("bshhhhhhhhhh");
 	}
